33e7c18d46998309015667fd253e1761c7c58a90
Chen Xiaohui <ysucxh@163.com>
2024-04-19 20:38:41 +0800

    database, web, mail

---
+++ examples/gawk/advanced/bowling.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+for team in $(gawk -F, '{print $2}' scores.txt | uniq)
+do
+    gawk -v team=$team '
+    BEGIN {
+        FS=","
+        total = 0
+    }
+    $2 == team {
+        total += $3 + $4 + $5
+    }
+    END {
+        avg = total / 6
+        print "Total for", team, "is", total, ", the average is", avg
+    }
+    ' scores.txt
+
+done
